5ada45f28648f2e8947cdc33247fcd3fb35d7819
Chaitanya girgaonkar <94917737+chaitanyagirgaonkar@users.noreply.github.com>
2024-04-15 12:57:55 +0530

    Update app.js

--- backend_sm/src/app.js
+++ backend_sm/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: ["https://eduscribee.vercel.app/"],
+    origin: ["https://eduscribee.vercel.app"],
     credentials: true
 }))
 
